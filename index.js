cf0f5c049f3d676592416d100711d7c03a6a747e
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-24 13:55:35 -0400

    Added files from repl

---
+++ index.js
@@ -0,0 +1,24 @@
+/*$(function() {
+$('.thumbnail').on('click', e => {
+  console.log(e.target);
+  const selectedImage = $(event.currentTarget);
+  const selectedSource = $(selectedImage)
+  const otherImage = $('.thumbnail').not(selectedImage);
+  const pressedBool = $(selectedImage).attr('aria-pressed') === 'true';
+  //SOMETHING HERE TO MAKE THE PROCESS KEYBOARD ACCESSIBLE//
+  //$(selectedImage).addClass('hero');//
+  $('div.hero').empty();
+  $(selectedImage).clone().appendTo('div.hero').attr(img);
+  //$(selectedImage).addClass('hero');//
+  });
+});*/
+
+$(function () {
+  $('.thumbnail').on('click', e => {
+    e.preventDefault();
+    });
+  let thumbSrcAttr = $(event.currentTarget).find('img').attr('src');
+  let thumbAltAttr = $(event.currentTarget).find('img').attr('alt');
+  $('.hero img').attr('src', thumbSrcAttr);
+  $('.hero img').attr('alt', thumbAltAttr);
+});
